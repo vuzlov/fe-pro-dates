f53c8bef6e827b8e8b190a0a146261a5f215ac54
evgen_vuzlov <evuzlov@gmail.com>
2022-09-26 16:50:46 +0300

    done-hw11

--- js/dates.js
+++ js/dates.js
@@ -23,12 +23,20 @@ function getDay(date, lang) {
       6: 'Суббота',
     },
   };
+  // первый вариант 
+  /*return(new Intl.DateTimeFormat(lang, options={ weekday: 'long'}).format(date));*/
+  // второй вариант
+  return(dayNames[lang][date.getDay()]);
 }
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
-function formatTime(date) {}
+function formatTime(date) {
+  let h =date.getHours().toString().padStart(2,'0');
+  let m =date.getMinutes().toString().padStart(2,'0');
+return(`${h}:${m}`);
+}
 
 /*
 Напишите функцию getLastDayOfMonth(year, month), 
@@ -39,7 +47,10 @@ year – год из четырёх цифр, например, 2012.
 month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
-function getLastDayOfMonth(year, month) {}
+function getLastDayOfMonth(year, month) {
+  let date =new Date(year,month +1,0);
+ return(date.getDate());
+};
 
 module.exports = {
   getDay,
